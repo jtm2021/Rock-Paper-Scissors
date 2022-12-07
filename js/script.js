4e1abf1162df9e2d13fb59d4b8b91d7d83b6a5e9
jtm2021 <91266239+jtm2021@users.noreply.github.com>
2022-12-07 10:53:26 +0000

    Fix bug: arrange location of index.html file in the directory

---
+++ js/script.js
@@ -0,0 +1,116 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function welcome() {
+      let playerName = prompt("Welcome to the Rock-Paper-Scissors game! What is your name?");
+      player = playerName?.toLowerCase().trim() || '';
+        if (player) {
+            alert(`\nHello ${player.trim()}!\n\nHere are the rules:\n1. Rock beats Scissors, Scissors beats Paper, Paper beats Rock.\n2. You can only choose the number corresponding to the choices.\n3. The computer will not play if your answer is invalid.\n\nSo, Let's play!`);
+            game();   
+        } else if (player === null) {
+            alert("You did not enter your name. It seems like you're not interested. Goodbye!");
+        } else {
+            alert("You did not enter your name. It seems like you're not interested. Goodbye!");
+        }
+}
+
+function computerPlay() {
+    let computerSelection = Math.floor(Math.random() * 3);   
+    if (computerSelection === 0) {
+        computerSelection = "rock";
+    } else if (computerSelection === 1) {
+        computerSelection = "paper";
+    } else {
+        computerSelection = "scissors";
+    }
+    return computerSelection;
+}
+
+function playerPlay() {
+    let playerSelection = prompt("Enter your choice (Just pick a number!):\n 1 = ROCK\n 2 = PAPER\n 3 = SCISSORS");
+    if (playerSelection !== "1" && playerSelection !== "2" && playerSelection !== "3") {
+        return;
+    } else if (playerSelection == "1") {
+        playerSelection = "rock";
+        return playerSelection;
+    } else if (playerSelection == "2") {
+        playerSelection = "paper";
+        return playerSelection;
+    } else {
+        playerSelection = "scissors";
+        return playerSelection;
+    } 
+}
+
+function playRound(answer1, answer2) {
+    if (answer1 == answer2){
+        return("It's a tie for this round!");
+    }
+    if(answer1 == "rock"){
+        if(answer2 == "scissors"){
+            playerScore++;
+            return("You win! Rock beats Scissors");
+        }
+        else{
+            computerScore++;
+            return("Computer wins! Paper beats Rock");
+        }
+    }
+    if(answer1 == "paper"){
+        if(answer2 == "rock"){
+            playerScore++;
+            return ("You win! Paper beats Rock");
+        }
+        else {
+            computerScore++;
+            return ("Computer wins! Scissors beats Paper");
+        }
+    }
+    if(answer1 == "scissors"){
+        if(answer2 == "paper"){
+            playerScore++;
+            return ("You win! Scissors beats Paper");
+        }
+        else {
+            computerScore++;
+            return ("Computer wins! Rock beats Scissors");
+        }
+    }
+}
+
+function game() {
+    for (let i = 1; i < 6; i++) {
+        console.log("\n------ROUND" + " " + i + "------\n");
+        let userChoice = playerPlay();
+        let computerChoice = computerPlay();
+        
+        if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors" && userChoice !== null){
+            console.log("You didn't choose a valid option. No score for this round :(\n");
+        }
+        else {
+            console.log("You chose" + " " + userChoice);        
+            console.log("The computer chose" + " " + computerChoice);
+            console.log("\n" + playRound(userChoice, computerChoice));
+            console.log("\n -----SCOREBOARD-----" + "\n| You     :" + playerScore + "         |\n"+ "| Computer:" + computerScore + "         |\n --------------------");
+        }
+    }
+    if (playerScore > computerScore){
+        console.log("----------------------------\nCongratulations! You win!\n----------------------------")
+        alert("\nCongratulations! You win!\n\n Thanks for playing!")
+    }
+    else if (playerScore < computerScore){
+        console.log("--------------------------------------\nComputer wins! Better Luck Next Time :)\n--------------------------------------");
+        alert("\nComputer wins! Better Luck Next Time :)\n\n Thanks for playing!")
+    }
+    else if (playerScore === 0 && computerScore === 0){
+        console.log("------------------------------------------\nSorry no one is not interested to play! :(\n------------------------------------------");
+        alert("\nSorry no one is not interested to play! :(")
+    }
+    else {
+        console.log("---------------------\nWow it's a tie!\n---------------------")
+        alert("\nWow it's a tie!\n\n Thanks for playing!")
+    }
+}
+
+            
+welcome();
